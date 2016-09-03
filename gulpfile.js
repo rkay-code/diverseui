409a359eb6836cfb0763fb6a1d2321eecc79f097
reneepadgham <renee@padgham.com>
2016-09-02 21:24:56 -0700

    gulpfile edit

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,16 @@ var open = require('gulp-open');
 gulp.task('transform', function () {
   return gulp.src('*.jsx')
         .pipe(react({harmony: false, es6module: true}))
-        .pipe(gulp.dest('output'));
+        .pipe(gulp.dest('out'));
 });
 
 gulp.task('es6', ['transform'], function () {
-  return gulp.src('output/*.js')
+  return gulp.src('out/*.js')
         .pipe(babel())
-        .pipe(gulp.dest('output'));
+        .pipe(gulp.dest('out'));
 });
 
 gulp.task('build',['es6'], function(){
   return gulp.src('./index.html')
-        .pipe(open(), {app: 'google-chrome'});
+        .pipe(gulp.dest('out'));
 });
