15c3b9a821d3e6161af0142873c30d47d70e0b4a
reneepadgham <renee@padgham.com>
2016-09-02 21:36:54 -0700

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,16 @@
 var gulp  = require('gulp');
-var react = require('gulp-react');
 var babel = require('gulp-babel');
 var open = require('gulp-open');
 
-gulp.task('transform', function () {
-  return gulp.src('*.jsx')
-        .pipe(react({harmony: false, es6module: true}))
-        .pipe(gulp.dest('out'));
+gulp.task('js', function () {
+  return gulp.src('*.js')
+    .pipe(babel({
+      plugins: ['transform-react-jsx']
+    }))
+    .pipe(gulp.dest('output'));
 });
 
-gulp.task('es6', ['transform'], function () {
-  return gulp.src('out/*.js')
-        .pipe(babel())
-        .pipe(gulp.dest('out'));
-});
-
-gulp.task('build',['es6'], function(){
+gulp.task('build', ['js'], function(){
   return gulp.src('./index.html')
-        .pipe(gulp.dest('out'));
+    .pipe(open(), {app: 'google-chrome'});
 });
