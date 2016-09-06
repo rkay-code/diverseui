fb9927ca44badf8c8511faf3559210e2dce9d2b2
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-09-05 19:27:36 -0700

    Add load more button

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
+  var PER_PAGE = 50;
   var IMAGES = window.Data || [];
 
   var size = 90;
   var gender = 'both';
+  var count = PER_PAGE;
 
-  var showImages = function(size, gender) {
+  var showImages = function() {
     var images = _.filter(IMAGES, function(image) {
       return gender === 'both' || image.gender === gender;
-    }).slice(0, 50);
+    }).slice(0, count);
 
     var imageNodes = _.map(images, function(i) {
       return '<img width="' + size + '" height="' + size + '" src="' + i.url + '" />';
@@ -16,10 +18,20 @@ $(document).ready(function() {
     $('#images').html(imageNodes.join(''));
   };
 
+  $('#load-more').on('click', function() {
+    count += PER_PAGE;
+
+    if (count >= IMAGES.length) {
+      $('#load-more').remove();
+    }
+
+    showImages();
+  });
+
   $('input[type=radio][name="gender"]').change(function() {
     gender = this.value;
 
-    showImages(size, gender);
+    showImages();
   });
 
   $('#size-slider').slider({
@@ -31,7 +43,8 @@ $(document).ready(function() {
     slide: function(event, ui) {
       $('#slider-label').html(ui.value + 'px');
       size = ui.value;
-      showImages(size, gender);
+
+      showImages();
     }
   });
 
