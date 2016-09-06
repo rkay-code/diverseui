34d5974b998a291d6b505464523cc20ca7624c9f
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-09-05 19:12:32 -0700

    Integrate frontend with backend

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,15 @@
 var gulp  = require('gulp');
-var connect = require('gulp-connect');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
-gulp.task('connect', function() {
-  return connect.server({
-    livereload: true,
-    port: 8080
-  });
-});
-
-gulp.task('watch', function() {
-  return gulp.watch(['scripts/*.js', '*.html', 'styles/*.css'], ['stream']);
-});
-
 gulp.task('sass', function() {
-  return gulp.src('styles/*.scss')
+  return gulp.src('static/styles/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('styles'));
+    .pipe(gulp.dest('static/styles'));
 });
 
 gulp.task('sass:watch', function() {
-  gulp.watch('styles/*.scss', ['sass']);
+  gulp.watch('static/styles/*.scss', ['sass']);
 });
 
-gulp.task('stream', function() {
-  return gulp.src('scripts/*.js')
-    .pipe(gulp.dest('output'))
-    .pipe(connect.reload());
-});
-
-gulp.task('default', ['connect', 'sass', 'sass:watch', 'stream', 'watch']);
+gulp.task('default', ['sass', 'sass:watch']);
--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,5 +1,39 @@
 $(document).ready(function() {
-  window.Data.slice(0, 5).forEach(function(image) {
-    $('body').append('<img height="50" width="50" src="' + image.url + '" />');
+  var IMAGES = window.Data || [];
+
+  var size = 90;
+  var gender = 'both';
+
+  var showImages = function(size, gender) {
+    var images = _.filter(IMAGES, function(image) {
+      return gender === 'both' || image.gender === gender;
+    }).slice(0, 50);
+
+    var imageNodes = _.map(images, function(i) {
+      return '<img width="' + size + '" height="' + size + '" src="' + i.url + '" />';
+    });
+
+    $('#images').html(imageNodes.join(''));
+  };
+
+  $('input[type=radio][name="gender"]').change(function() {
+    gender = this.value;
+
+    showImages(size, gender);
   });
+
+  $('#size-slider').slider({
+    range: 'min',
+    value: size,
+    min: 32,
+    max: 180,
+    step: 1,
+    slide: function(event, ui) {
+      $('#slider-label').html(ui.value + 'px');
+      size = ui.value;
+      showImages(size, gender);
+    }
+  });
+
+  showImages(size, gender);
 });
