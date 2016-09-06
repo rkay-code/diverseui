bf77fb51321a13e0b395005c787d0cc0c7d9c299
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-09-05 23:40:54 -0700

    Add secret key

--- app.py
+++ app.py
@@ -89,5 +89,6 @@ def index():
                            images=[image.to_json() for image in images])
 
 if __name__ == '__main__':
+    app.secret_key = os.environ.get('SECRET_KEY', 'somethingsecret')
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
