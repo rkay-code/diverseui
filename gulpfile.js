c8c036e82d1485c416f56764c2292a6f7f61438b
reneepadgham <renee@padgham.com>
2016-09-07 14:11:58 -0700

    create css for all new sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp  = require('gulp');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
 gulp.task('sass', function() {
-  return gulp.src('static/styles/styles.scss')
+  return gulp.src('static/styles/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
+    .pipe(rename(function (path) {
+    path.basename += "-compiled";
+    path.extname = ".css"
+    }))
     .pipe(gulp.dest('static/styles'));
 });
 
