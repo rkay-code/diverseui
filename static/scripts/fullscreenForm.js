d2f82283ce1e7656c633c0f71b1773f34b4b15a4
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-09-07 23:00:01 -0700

    Enforce http take 2

--- static/scripts/fullscreenForm.js
+++ static/scripts/fullscreenForm.js
@@ -417,7 +417,7 @@
 				}
 				break;
 			case 'url':
-				var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+				var expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi
 				var r = new RegExp(expression);
 				if (input.value === '') {
 					error = 'NOURL';
