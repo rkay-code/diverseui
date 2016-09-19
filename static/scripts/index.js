bc4e962e84fc63b82a5c4423f5121350ca85cac6
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-09-18 20:32:44 -0700

    Add download selected and download all functionality

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -17,7 +17,15 @@ $(document).ready(function() {
     var images = filtered.slice(0, count);
 
     var imageNodes = _.map(images, function(i) {
-      return '<img width="' + size + '" height="' + size + '" src="' + i.url + '" />';
+      return ([
+        '<img ',
+          'crossOrigin="Anonymous" ',
+          'class="image" ',
+          'width="', size,
+          '" height="', size,
+          '" src="', i.url, '"',
+        ' />'
+      ].join(''));
     });
 
     $('#images').html(imageNodes.join(''));
@@ -54,5 +62,51 @@ $(document).ready(function() {
 
       showImages();
     }
+  });
+
+  var canvas = document.createElement('canvas');
+  var context = canvas.getContext('2d');
+
+  var getFolderName = function() {
+    var now = new Date();
+    var month = ('0' + (now.getMonth() + 1)).slice(-2);
+    var date = ('0' + now.getDate()).slice(-2);
+    return ['diverseui', month, date].join('-');
+  };
+
+  var downloadImages = function(images) {
+    var zip = new JSZip();
+    var i;
+    var imageData;
+    var folder = getFolderName();
+
+    canvas.width = size;
+    canvas.height = size;
+
+    for (i = 0; i < images.length; i++) {
+      context.drawImage(images[i], 0, 0, size, size);
+      imageData = canvas.toDataURL().replace(/^data:image\/(png|jpg);base64,/, '');
+      zip.file(folder + '/image-' + (i + 1) + '.png', imageData, {base64: true});
+    }
+
+    zip.generateAsync({type: 'blob'}).then(function(blob) {
+      saveAs(blob, folder + '.zip');
+    }, function(err) {
+      console.log(err);
+    });
+  }
+
+  $('.image').on('click', function() {
+    $(this).toggleClass('selected-image');
+  });
+
+  $('#download-all').on('click', function() {
+    var images = document.getElementsByClassName('image');
+    downloadImages(images);
+  });
+
+  $('#download-selected').on('click', function() {
+    var images = document.getElementsByClassName('selected-image');
+    downloadImages(images);
   });
 });
