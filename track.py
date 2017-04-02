08fa76bb5810298eda47273d708e7e45fee19418
Yefim Vedernikoff <yefim323@gmail.com>
2017-04-01 23:12:52 -0700

    Track gender labels betterg

--- track.py
+++ track.py
@@ -2,8 +2,14 @@ from google_measurement_protocol import Event, report
 import uuid
 
 
+GENDERS = {
+    'female': 'Gender Female',
+    'male': 'Gender Male'
+}
+
+
 def log_fetch(count, gender):
-    gender = "Gender %s" % gender
+    label = GENDERS.get(gender, 'Gender Neutral')
     client_id = uuid.uuid4()
-    event = Event('API', 'Fetch', label=gender, value=count)
+    event = Event('API', 'Fetch', label=label, value=count)
     report('UA-68765997-3', client_id, event)
