3f47716c80bff2306fe1968aab1b2731ec3ae77c
Yefim Vedernikoff <yefim323@gmail.com>
2017-04-04 21:19:39 -0700

    Add menu

--- static/scripts/menu.js
+++ static/scripts/menu.js
@@ -1,1 +1,11 @@
-var openMenu = document.querySelector('.menu-button');
+var menuButton = document.querySelector('.menu-button');
+
+if (menuButton) {
+  menuButton.addEventListener('click', function() {
+    if (document.body.className === 'site') {
+      document.body.className = 'site open';
+    } else {
+      document.body.className = 'site';
+    }
+  });
+}
