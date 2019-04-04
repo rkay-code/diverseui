b3ed0cf6be60db318c9a16f491383533c38b7fa6
Yefim Vedernikoff <yefim323@gmail.com>
2019-04-04 03:03:16 -0400

    Ordinary calling format

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import os
 import boto
 import boto.s3
 from boto.s3.key import Key
+from boto.s3.connection import OrdinaryCallingFormat
 import requests
 import uuid
 
@@ -14,8 +15,11 @@ def upload_url_to_s3(image_url):
 
     fname = str(uuid.uuid4())
 
-    conn = boto.connect_s3(os.environ['AWS_ACCESS_KEY_ID_DIVERSEUI'],
-                           os.environ['AWS_SECRET_KEY_DIVERSEUI'])
+    conn = boto.connect_s3(
+        aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID_DIVERSEUI'],
+        aws_secret_access_key = os.environ['AWS_SECRET_KEY_DIVERSEUI'],
+        calling_format = OrdinaryCallingFormat(),
+    )
     bucket = conn.get_bucket('static.diverseui.com')
 
     k = Key(bucket, fname)
